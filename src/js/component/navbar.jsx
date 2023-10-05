01b577bcaf039ce4491fe289c294d3af28906a1f
FrancoRegg <138858381+FrancoRegg@users.noreply.github.com>
2023-10-05 13:58:44 +0000

    ok

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -3,16 +3,27 @@ import { Link } from "react-router-dom";
 import "../../styles/navbar.css";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand h1 text-center">StarWars</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light ">
+      <Link to="/">
+        <img className="imgNav" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSquUoilY01uGHYZ7mRlJRqhcRQ5qJVOnIM0A&usqp=CAU"  alt="" />
+      </Link> 
+      <div className="ml-auto">
+        <Link to="/demo"></Link>
+      </div>
+      <div className="cTitulos">
+        <Link>
+          <span className="title">CHARACTERS</span>
+        </Link>
+        <Link>
+          <span className="title">PLANETS</span>
+        </Link>
+        <Link>
+          <span className="title">VEHICLES</span>
+        </Link>
+      </div>
+    </nav>
+  );
 };
+//<span className="navbar-brand h1 text-center">StarWars</span>
+//<button className="btn btn-primary">Check the Context in action</button>
