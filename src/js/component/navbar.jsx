9e696216799d6df368e28466bee91c829676b8fa
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-05 20:31:46 +0000

    Se esta haciendo responsive

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -3,81 +3,96 @@ import { Link } from "react-router-dom";
 import "../../styles/navbar.css";
 import { Context } from "../store/appContext";
 
-
 export const Navbar = () => {
   const { store } = useContext(Context);
   console.log("STORE", store);
+
   return (
     <>
-
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container">
-          <div className="contnav collapse navbar-collapse" id="navbarSupportedContent">
-            <Link to="/">
-              <img className="imgNav" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSquUoilY01uGHYZ7mRlJRqhcRQ5qJVOnIM0A&usqp=CAU" alt="" />
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarSupportedContent"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent"></div>
-            <div className="ml-auto">
-              <Link to="/demo"></Link>
-            </div>
+        <div className="container-fluid contnav">
+          <Link to="/">
+            <img
+              className="imgNav"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSquUoilY01uGHYZ7mRlJRqhcRQ5qJVOnIM0A&usqp=CAU"
+              alt=""
+            />
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <div className="Titulos">
-              <Link to="/peoples"><span className="title btn ">Characters</span></Link>
-              <Link to="/planets"><span className="title btn ">Planets</span></Link>
-              <Link to="/vehicles"><span className="title btn ">Vehicles</span></Link>
-              <Link to="/favorites"><span className="title btn ">Favorites {store.favorites.length}</span></Link>
+              <Link to="/peoples">
+                <span className="title btn ">Characters</span>
+              </Link>
+              <Link to="/planets">
+                <span className="title btn ">Planets</span>
+              </Link>
+              <Link to="/vehicles">
+                <span className="title btn ">Vehicles</span>
+              </Link>
+              <Link to="/favorites">
+                <span className="title btn ">
+                  Favorites {store.favorites.length}
+                </span>
+              </Link>
             </div>
           </div>
         </div>
       </nav>
-
     </>
-  )
-}
+  );
+};
 
+/* 
+<nav className="navbar navbar-expand-sm bg-body-tertiary">
+        <div className="container-fluid">
+          <div
+            className="contnav collapse navbar-collapse"
+            id="navbarSupportedContent"
+          >
+            <Link to="/">
+              <img
+                className="imgNav"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSquUoilY01uGHYZ7mRlJRqhcRQ5qJVOnIM0A&usqp=CAU"
+                alt=""
+              />
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <div className="Titulos">
+                <Link to="/peoples">
+                  <span className="title btn ">Characters</span>
+                </Link>
+                <Link to="/planets">
+                  <span className="title btn ">Planets</span>
+                </Link>
+                <Link to="/vehicles">
+                  <span className="title btn ">Vehicles</span>
+                </Link>
+                <Link to="/favorites">
+                  <span className="title btn ">
+                    Favorites {store.favorites.length}
+                  </span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+*/
 
+/*
 
-/*<div class="dropdown">
-          <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-
-          <ul class="dropdown-menu">
-          {store.favorites.map((fav, index) => (
-              <li key={index}>
-                <a className="dropdown-item" href="#">
-                {fav}</a>
-              </li>
-              ))}
-          </ul>
-        </div>*/
-
-//<div className="navbar-brand h1 text-center">StarWars</div>
-//<button className="btn ">Check the Context in action</button>
-
-/*<nav className="navbar navbar-light ">
-      <Link to="/">
-        <img className="imgNav" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSquUoilY01uGHYZ7mRlJRqhcRQ5qJVOnIM0A&usqp=CAU"  alt="" />
-      </Link> 
-      <div className="ml-auto">
-        <Link to="/demo"></Link>
-      </div>
-      <div className="cTitulos">
-        <Link to="/peoples">
-          CHARACTERS</div>
-        </Link>
-        <Link to="/planets">
-          PLANETS</div>
-        </Link>
-        <Link to="/vehicles">
-          VEHICLES</div>
-        </Link>
-      </div>
-    </nav>*/
+*/
